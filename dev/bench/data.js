7ac5f94de31624ea4f92e9a13df0ce2d1c8a145a
github-action-benchmark <github@users.noreply.github.com>
2026-01-05 06:32:56 +0000

    add Rust Benchmark (customSmallerIsBetter) benchmark result for 0fdc989e82412bf861eff65f1ab818a705048717

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1767587148892,
+  "lastUpdate": 1767594776175,
   "repoUrl": "https://github.com/Na1w/infinitedsp",
   "entries": {
     "Rust Benchmark": [
@@ -23,6 +23,55 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/Na1w/infinitedsp/commit/937c5ee143ebcb56b3cb7b657a2c543193905c75"
         },
         "date": 1767587148510,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "dsp_benchmarks::oscillator::bench_oscillator_sine",
+            "value": 40220,
+            "unit": "instructions"
+          },
+          {
+            "name": "dsp_benchmarks::oscillator::bench_oscillator_saw",
+            "value": 21333,
+            "unit": "instructions"
+          },
+          {
+            "name": "dsp_benchmarks::oscillator::bench_oscillator_square",
+            "value": 51906,
+            "unit": "instructions"
+          },
+          {
+            "name": "dsp_benchmarks::oscillator::bench_oscillator_noise",
+            "value": 20775,
+            "unit": "instructions"
+          },
+          {
+            "name": "dsp_benchmarks::reverb::bench_reverb",
+            "value": 313818,
+            "unit": "instructions"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fredrikandersson@mac.com",
+            "name": "Fredrik Andersson",
+            "username": "Na1w"
+          },
+          "committer": {
+            "email": "fredrikandersson@mac.com",
+            "name": "Fredrik Andersson",
+            "username": "Na1w"
+          },
+          "distinct": true,
+          "id": "0fdc989e82412bf861eff65f1ab818a705048717",
+          "message": "Add documentation build for gh-pages",
+          "timestamp": "2026-01-05T07:31:17+01:00",
+          "tree_id": "45535a3dc853a447f54c265f56639a53f2692e22",
+          "url": "https://github.com/Na1w/infinitedsp/commit/0fdc989e82412bf861eff65f1ab818a705048717"
+        },
+        "date": 1767594775347,
         "tool": "customSmallerIsBetter",
         "benches": [
           {

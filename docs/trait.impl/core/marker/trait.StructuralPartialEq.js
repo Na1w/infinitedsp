f9acc4329a9b302978d295bb89082acc88e012e5
Na1w <Na1w@users.noreply.github.com>
2026-03-04 17:44:29 +0000

    deploy: 0de4538d48e124d24f876608e8e4f7fbe0a71eb0

--- docs/trait.impl/core/marker/trait.StructuralPartialEq.js
+++ docs/trait.impl/core/marker/trait.StructuralPartialEq.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["infinitedsp_core",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.0/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"enum\" href=\"infinitedsp_core/synthesis/envelope/enum.AdsrState.html\" title=\"enum infinitedsp_core::synthesis::envelope::AdsrState\">AdsrState</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.0/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"enum\" href=\"infinitedsp_core/synthesis/lfo/enum.LfoWaveform.html\" title=\"enum infinitedsp_core::synthesis::lfo::LfoWaveform\">LfoWaveform</a>"]]]]);
+    var implementors = Object.fromEntries([["infinitedsp_core",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.1/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"enum\" href=\"infinitedsp_core/synthesis/envelope/enum.AdsrState.html\" title=\"enum infinitedsp_core::synthesis::envelope::AdsrState\">AdsrState</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.1/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"enum\" href=\"infinitedsp_core/synthesis/lfo/enum.LfoWaveform.html\" title=\"enum infinitedsp_core::synthesis::lfo::LfoWaveform\">LfoWaveform</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.1/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"enum\" href=\"infinitedsp_core/synthesis/oscillator/enum.Waveform.html\" title=\"enum infinitedsp_core::synthesis::oscillator::Waveform\">Waveform</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[722]}
+//{"start":57,"fragment_lengths":[1075]}
